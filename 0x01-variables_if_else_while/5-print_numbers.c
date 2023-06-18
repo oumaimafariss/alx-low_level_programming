6a6f37066c263aad28b574cb9d467e5f2547bb90
oumaimafariss <oumaima891.of@gmail.com>
2023-06-18 06:39:09 -0700

    add all files

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabt in lowercase except e and q
+ *
+ * Return:  0.
+*/
+int main(void)
+{
+	int nb;
+
+	for (nb = 0; nb < 10; nb++)
+		printf("%d",nb);
+	
+	printf("\n");
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers of base 10 starting from 0
+ * only using putchar
+ *
+ * Return:  0.
+*/
+int main(void)
+{
+	int nb;
+
+	for (nb = 0; nb < 10; nb++)
+		putchar((nb % 10) + '0');
+	
+	putchar('\n');
+
+	return (0);
+}
