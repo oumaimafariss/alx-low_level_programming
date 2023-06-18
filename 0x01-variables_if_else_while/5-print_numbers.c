2860a045d031f03b200609c5530048bd67cd5b8b
oumaimafariss <oumaima891.of@gmail.com>
2023-06-18 07:25:19 -0700

    all files last version

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print the alphabt in lowercase except e and q
+ * main - prints all sigle digit numbers of base 10 starting from 0
  *
  * Return:  0.
 */
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ *
+ * Return:  0.
+*/
+int main(void)
+{
+	int nb;
+
+	for (nb = 0; nb < 10; nb++)
+	{
+		putchar((nb % 10) + '0');
+		if (nb == 9)
+			continue;
+		putchar(' ');
+		putchar(',');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
