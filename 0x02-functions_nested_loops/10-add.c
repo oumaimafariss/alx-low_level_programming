e73012698f6443bb44df683d840bf91d87f178a0
oumaimafariss <oumaima891.of@gmail.com>
2023-06-21 13:35:28 -0700

    10-files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @a: the first number in the addiction
+ * @b: the second number in the addiction
+ *
+ * Return: the sum of the integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
