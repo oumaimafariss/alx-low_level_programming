a4b601696f46043e629462c85fb76d7eba2f57e0
oumaimafariss <oumaima891.of@gmail.com>
2023-06-21 12:57:51 -0700

    2-files

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *                     followed by an new line
+ *
+ *Return: no return value (void)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char letter;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+		_putchar('\n');
+	}
+}
